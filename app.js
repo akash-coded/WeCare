5d140eb40aeb5d150f91c2a0ac6ae37708ea7d17
Akash Das <mfs.akash@gmail.com>
2020-11-03 12:57:06 +0530

    Added custome error handler

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const winston = require("winston");
-// const logger = require("./helpers/logger");
+// const { logger } = require("./helpers/logger");
 const { handleError, ErrorHandler } = require("./helpers/error");
 
 // convert the body of incoming requests into JavaScript objects if express version < 4.16
@@ -116,6 +116,23 @@ app.get("/users/:userId", (req, res) => {
   res.status(200).send(req.params);
 });
 
+app
+  .route("/users/booking/:bookingId")
+  .put((req, res) => {
+    res.status(200).send(req.params);
+  })
+  .delete((req, res) => {
+    res.status(200).send(req.params);
+  });
+
+app.get("/users/booking/:userId", (req, res) => {
+  res.status(200).send(req.params);
+});
+
+app.post("/users/booking/:userId/:coachId", (req, res) => {
+  res.status(200).send(req.params);
+});
+
 app.post("/coaches", (req, res) => {
   res.status(200).send("WeCare: Extending Care For Devs");
 });
@@ -132,24 +149,7 @@ app.get("/coaches/:coachId", (req, res) => {
   res.status(200).send(req.params);
 });
 
-app.post("/users/booking/:userId/:coachId", (req, res) => {
-  res.status(200).send(req.params);
-});
-
-app
-  .route("/users/booking/:bookingId")
-  .put((req, res) => {
-    res.status(200).send(req.params);
-  })
-  .delete((req, res) => {
-    res.status(200).send(req.params);
-  });
-
 app.get("/coaches/booking/:coachId", (req, res) => {
-  res.status(200).send(req.params);
-});
-
-app.get("/users/booking/:userId", (req, res) => {
   res.status(200).send(req.params);
 });
 
--- helpers/error.js
+++ helpers/error.js
@@ -1,3 +1,8 @@
+const fs = require("fs");
+const { promisify } = require("util");
+
+const appendFile = promisify(fs.appendFile);
+
 class ErrorHandler extends Error {
   constructor(statusCode, message) {
     super();
@@ -8,6 +13,12 @@ class ErrorHandler extends Error {
 
 const handleError = (err, res) => {
   const { statusCode, message } = err;
+  (async () => {
+    await appendFile(
+      "./logs/errorResponse.log",
+      `[${new Date()}] error: ${statusCode} - ${message}\n`
+    );
+  })();
   res.status(statusCode).json({
     status: "error",
     statusCode,
